af45ebf106b52089b759b39b820d745258ede62b
Adam O <adamorr234@gmail.com>
2021-03-14 15:48:27 -0600

    Success!

--- index.js
+++ index.js
@@ -3,21 +3,22 @@ require('dotenv').config();
 const express = require('express');
 //require app Routers
 const deviceInfoDetectorRouter = require("device-info-detector");
-//const advancednodeRouter = require("boilerplate-advancednode/")
+const advancednodeRouter = require("fcc-advanced-node-and-express")
 
 //universal app middleware
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
 /* change code below this line */
 // app.use('/public', express.static(process.cwd() + '/public'));
 //get Routers and map them to routes
-//app.use("/advancednode", advancednodeRouter);
+
 app.get("/", function(req, res) {
   res.send("You have reached the root of nodejs-demos by Adam O. More content coming soon!");
 })
 app.use("/device-info-detector", deviceInfoDetectorRouter);
+app.use("/advancednode", advancednodeRouter);
